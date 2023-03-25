abd00ed35a94e345f868660c460dfc6023feb1e9
a.timofeev <tauman08@gmail.com>
2023-03-25 13:30:09 +0300

    lesson_9

--- blog/config.py
+++ blog/config.py
@@ -1,13 +1,23 @@
+import os 
 class BaseConfig:
     DEBUG = False
     DATABASE_URI = ''
     SECRET_KEY = '_5#y2L"F4Q8zFGFGDScvvx54687'
+    basedir = os.path.abspath(os.path.dirname(__file__))
 
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///'+os.path.join(basedir, 'db.sqlite')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+
     WTF_CSRF_ENABLED = True
-    FLASK_ADMIN_SWATCH = 'journal'
+
+    FLASK_ADMIN_SWATCH = 'pulse'
+
+    OPENAPI_URL_PREFIX = '/api/docs'
+    OPENAPI_VERSION = '3.0.0'
+    OPENAPI_SWAGGER_UI_PATH = '/'
+    OPENAPI_SWAGGER_UI_VERSION = '3.51.1'  # see version on https://cdnjs.com/libraries/swagger-ui
+
 
 class Development(BaseConfig):
     DEBUG = True
