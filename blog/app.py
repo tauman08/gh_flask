eacd86004170f9ca3d38a0ee2360d537aeeb78aa
Aleksandr Timofeev <41984641+tauman08@users.noreply.github.com>
2023-05-13 12:27:30 +0300

    lesson_10 (#10)

--- blog/app.py
+++ blog/app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from combojsonapi.event import EventPlugin
+from combojsonapi.permission import PermissionPlugin
 from combojsonapi.spec import ApiSpecPlugin
 from blog import commands
 from blog.extensions import db, login_manager, migrate, csrf, admin, api
@@ -9,7 +11,7 @@ from blog.config import Development
 def create_app() -> Flask:
     app = Flask(__name__)
     app.config.from_object(Development)
-    #app.config.from_object('blog.config')
+    # app.config.from_object('blog.config')
 
     register_extensions(app)
     register_blueprints(app)
@@ -19,12 +21,12 @@ def create_app() -> Flask:
 
 
 def register_extensions(app):
-    
+
     db.init_app(app)
-    migrate.init_app(app,db, compare_type=True)
+    migrate.init_app(app, db, compare_type=True)
     csrf.init_app(app)
     admin.init_app(app)
-    
+
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
@@ -35,12 +37,14 @@ def register_extensions(app):
 
 def register_api(app: Flask):
 
-    from blog.api.tag import TagList,TagDetail
-    from blog.api.user import UserList,UserDetail
-    from blog.api.author import AuthorList,AuthorDetail
-    from blog.api.article import ArticleList,ArticleDetail
+    from blog.api.tag import TagList, TagDetail
+    from blog.api.user import UserList, UserDetail
+    from blog.api.author import AuthorList, AuthorDetail
+    from blog.api.article import ArticleList, ArticleDetail
 
     api.plugins = [
+        EventPlugin(),
+        PermissionPlugin(),
         ApiSpecPlugin(
             app=app,
             tags={
@@ -52,17 +56,19 @@ def register_api(app: Flask):
         ),
     ]
     api.init_app(app)
-    api.route(TagList,'tag_list', '/api/tags/', tag = 'Tag')
-    api.route(TagDetail, 'tag_detail','/api/tags/<int:id>', tag='Tag')
+    api.route(TagList, 'tag_list', '/api/tags/', tag='Tag')
+    api.route(TagDetail, 'tag_detail', '/api/tags/<int:id>', tag='Tag')
 
     api.route(UserList, 'user_list', '/api/users/', tag='User')
     api.route(UserDetail, 'user_detail', '/api/users/<int:id>', tag='User')
 
     api.route(AuthorList, 'author_list', '/api/authors/', tag='Author')
-    api.route(AuthorDetail, 'author_detail', '/api/authors/<int:id>', tag='Author')
+    api.route(AuthorDetail, 'author_detail',
+              '/api/authors/<int:id>', tag='Author')
 
     api.route(ArticleList, 'article_list', '/api/articles/', tag='Article')
-    api.route(ArticleDetail, 'article_detail', '/api/articles/<int:id>', tag='Article')
+    api.route(ArticleDetail, 'article_detail',
+              '/api/articles/<int:id>', tag='Article')
 
 
 def register_blueprints(app: Flask):
@@ -84,9 +90,6 @@ def register_blueprints(app: Flask):
     admin.register_views()
 
 
-
 def register_commands(app: Flask):
     app.cli.add_command(commands.create_init_user)
     app.cli.add_command(commands.create_init_tags)
-
-
