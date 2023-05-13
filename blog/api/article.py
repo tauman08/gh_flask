82f80049961035fc52aec6abb678e02add69cb35
a.timofeev <tauman08@gmail.com>
2023-05-13 12:22:40 +0300

    lesson_10

--- blog/api/article.py
+++ blog/api/article.py
@@ -1,3 +1,4 @@
+from combojsonapi.event.resource import EventsResource
 from flask_combo_jsonapi import ResourceList, ResourceDetail
 
 from blog.extensions import db
@@ -5,7 +6,27 @@ from blog.models import Article
 from blog.schemas import ArticleSchema
 
 
+class ArticleListEvent(EventsResource):
+
+    def event_get_count(self):
+        return {'count': Article.query.count()}
+
+    def event_get_api_server(self):
+        return {'server': request.get('https://ifconfig.io/ip').text}
+
+    def event_post_count(self):
+        # просто для примера  как создать post. данный метод не работает
+        return {'method': request.method}
+
+
+class ArticleDetailEvent(EventsResource):
+
+    def event_get_count_by_author(self,  *args, **kwargs):
+        return {'count': Article.query.filter(Article.author_id == kwargs['id']).count()}
+
+
 class ArticleList(ResourceList):
+    events = ArticleListEvent
     schema = ArticleSchema
     data_layer = {
         'session': db.session,
@@ -14,6 +35,7 @@ class ArticleList(ResourceList):
 
 
 class ArticleDetail(ResourceDetail):
+    events = ArticleDetailEvent
     schema = ArticleSchema
     data_layer = {
         'session': db.session,
